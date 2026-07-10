0c1f7ace28db1eb7f5318e6aa5873afe1999ead1
ti-ecoparque <ti.cascavel@ecoparque.com.br>
2026-07-10 16:16:18 -0300

    funcoes5

--- pages/cenario_d.py
+++
@@ -1,6 +0,0 @@
-# cenarios/cenario_d.py TESTE ISOLADO
-import streamlit as st
-
-def renderizar_cenario_d(rm_para_conferencia="", pedidos=None, supabase=None, Skinner_status=None):
-    st.title("🎯 TESTE: Cenário D está Vivo!")
-    st.write("Se você está vendo esta mensagem, o cenario_d.py foi importado com sucesso!")
